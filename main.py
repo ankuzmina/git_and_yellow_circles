f9d6d1999905792dd3a13c4b5871619fae294d75
Ann <anna.kuzmina.07@gmail.com>
2023-12-08 17:30:23 +0300

    Complete task "Git и желтые окружности"

--- main.py
+++ main.py
@@ -1,17 +1,40 @@
+import random
 import sys
 
 from PyQt5 import uic, QtCore, QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
 
+class QPainter:
+    pass
+
 class MyWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('ui.ui', self)
         self.pushButton.clicked.connect(self.go)
+        self._is_paint = False
 
     def go(self):
-        pass
+        self._is_paint = True
+        self.update()
+
+    def paintEvent(self, a0):
+        if self._is_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_circle(qp)
+            qp.end()
+
+    def draw_circle(self, qp: QPainter) -> None:
+        color = QColor(255, 255, 0)
+        radius - random.randint(10, min(self.width(), self.height()) // 2)
+        x = random.randrange(radius, self.width() - radius)
+        y = random.randrange(radius, self.height() - radius)
+        qp.setBrush(color)
+        qp.drawEllipse(x, y, radius, radius)
+
+
 
 def exception_hook(exctype, value, traceback):
     print(exctype, value, traceback)
